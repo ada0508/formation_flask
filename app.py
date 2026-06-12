7e9e94a31d2442d188632ddbe5dcdf282bb9e4bc
ada0508 <diopndeyeadama05@gmail.com>
2026-06-12 23:13:43 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
-from .env import load_dotenv # type: ignore
-import os
-load_dotenv()
-app = Flask(__name__)
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
     return render_template("index.html")
+
 @app.route("/about/")
 def about():
     return render_template("about.html")
